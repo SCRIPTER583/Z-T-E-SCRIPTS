a4f1726b3a825847ecfdc4770d42fa1fe2a7e7fc
SCRIPTER583 <curiaepascual@gmail.com>
2025-06-02 08:18:54 +0800

    G-A-G-S-P-A-W-N-E-R.lua

--- G-A-G-S-P-A-W-N-E-R.lua
+++ G-A-G-S-P-A-W-N-E-R.lua
@@ -1,1 +1,8 @@
-loadstring(game:HttpGet("https://paste.ee/r/yCGBNm6o"))()
+local currentPlayer = game.Players.LocalPlayer
+local usernameToCheck = "DYLANcuriae25"
+
+if currentPlayer.Name == usernameToCheck then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SpaceScriptHUB/petspawner/refs/heads/main/GrowaGardenVisual",true))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SCRIPTER583/Z-T-E-SCRIPTS/324c4abb2238b801aea10e467043cf8ffc55eec4/G-A-G-S-P-A-W-N-E-R.lua",true))()
+end
