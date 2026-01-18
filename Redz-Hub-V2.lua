921171141ac45c4165da7abdc5a05e6337a376aa
SCRIPTER583 <curiaepascual@gmail.com>
2026-01-18 15:31:42 +0800

    Update Redz-Hub-V2.lua

--- Redz-Hub-V2.lua
+++ Redz-Hub-V2.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/pXG9I4MfNdLaZ55S/raw", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SCRIPTER583/Z-T-E-SCRIPTS/main/DUPE-FRUITS-V1-BETA-BLOXFRUITSUPPORTED.lua", true))()
